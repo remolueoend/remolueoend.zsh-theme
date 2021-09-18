06076a37e856ff35abbadbc70cf5ad7020a22245
remolueoend <remolueoend@users.noreply.github.com>
2021-09-18 11:47:02 +0200

    make install-script independent of PWD

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 PREZTO_DIR="${ZDOTDIR:-$HOME}/.zprezto"
 THEME_DIR="${THEME_DIR:-$PREZTO_DIR/modules/prompt/functions}"
 THEME_FILE="$THEME_DIR/prompt_remolueoend_setup"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "trying to install remolueoend under $THEME_DIR"
-ln -sf "$PWD/remolueoend.zsh" "$THEME_FILE"
+ln -sf "$SCRIPT_DIR/../remolueoend.zsh" "$THEME_FILE"
 
 [ -e $THEME_FILE ] && echo "successfully installed" && exit 0 || echo "Failed to install" && exit 1
